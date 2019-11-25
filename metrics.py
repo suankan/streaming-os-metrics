57a8301e56734a6ed6d70d735da1cb9ad303672f
Suan Kan <5026704+suankan@users.noreply.github.com>
2019-11-26 01:27:22 +1100

    Simplified the class properties

--- metrics.py
+++ metrics.py
@@ -10,38 +10,26 @@ from datetime import datetime
 
 class Metrics():
     '''
-    This class gets OS Metrics and sends them to Kafka.
-    Metrics are obtained using psutil lib.
+    This class gets OS metrics using psutil lib.
     More info: https://psutil.readthedocs.io/en/latest/#psutil-documentation
     '''
     def __init__(self):
-        # Create a list of CPUs like ['cpu1', 'cpu2', ... , 'cpuN']
-        self.list_cpus = ['cpu' + str(item) for item in range(psutil.cpu_count(logical=True))]
-
-        self.metrics = {
-                'snapshot_started': time.time(),
-            'host_info': self.get_host_info(),
-            'cpu': {
-                'cpu_times': self.get_cpu_times(),
-                'cpu_percent': self.get_cpu_percent(),
-                'cpu_times_percent': self.get_cpu_times_percent(),
-                'cpu_stats': self.get_cpu_stats(),
-                'cpu_freq': self.get_cpu_freq(),
-                'load_average': self.get_load_average(),
-            },
-            'memory': {
-                'virtual_memory': self.get_virtual_memory(),
-                'swap_memory': self.get_swap_memory()
-            },
-            'disks': {
-                'disk_usage': self.get_disk_usage(),
-                'disk_io_counters': self.get_disk_io_counters()
-            },
-            'network': {
-                'net_io_counters': self.get_net_io_counters()
-            },
-                'snapshot_finished': time.time()
-            }
+        # Create a list of CPUs like ['cpu1', 'cpu2', ... , 'cpuN'] for further zipping
+        self.cpus = ['cpu' + str(item) for item in range(psutil.cpu_count(logical=True))]
+        self.snapshot_started = time.time()
+        self.host_info = self.get_host_info()
+        self.cpu_times = self.get_cpu_times()
+        self.cpu_percent = self.get_cpu_percent()
+        self.cpu_times_percent = self.get_cpu_times_percent()
+        self.cpu_stats = self.get_cpu_stats()
+        self.cpu_freq = self.get_cpu_freq()
+        self.load_average = self.get_load_average()
+        self.virtual_memory = self.get_virtual_memory()
+        self.swap_memory = self.get_swap_memory()
+        self.disk_usage = self.get_disk_usage()
+        self.disk_io_counters = self.get_disk_io_counters()
+        self.net_io_counters = self.get_net_io_counters()
+        self.snapshot_finished = time.time()
 
     def get_host_info(self):
         '''
@@ -58,15 +46,14 @@ class Metrics():
         '''
         # Zip together list of CPUs and their times .
         list_cpu_times = [item._asdict() for item in psutil.cpu_times(percpu=True)]
-        return dict(zip(self.list_cpus, list_cpu_times))
+        return dict(zip(self.cpus, list_cpu_times))
 
     def get_cpu_percent(self):
         '''
         Returns dict of CPU utilizations percentage for each CPU.
         '''
-        list_cpus = ['cpu' + str(item) for item in range(psutil.cpu_count(logical=True))]
         # Zip together list of CPUs and their utilisations.
-        return dict(zip(self.list_cpus, psutil.cpu_percent(interval=0.5, percpu=True)))
+        return dict(zip(self.cpus, psutil.cpu_percent(interval=0.5, percpu=True)))
 
     def get_cpu_times_percent(self):
         '''
@@ -74,7 +61,7 @@ class Metrics():
         '''
         # Zip together list of CPUs and their times percents.
         list_cpu_times_percents = [item._asdict() for item in psutil.cpu_times_percent(interval=0.5, percpu=True)]
-        return dict(zip(self.list_cpus, list_cpu_times_percents))
+        return dict(zip(self.cpus, list_cpu_times_percents))
 
     def get_cpu_stats(self):
         '''
@@ -88,7 +75,7 @@ class Metrics():
         '''
         # Zip together list of CPUs and their frequencies stats.
         list_cpu_freqs = [item._asdict() for item in psutil.cpu_freq(percpu=True)]
-        return dict(zip(self.list_cpus, list_cpu_freqs))
+        return dict(zip(self.cpus, list_cpu_freqs))
 
     def get_load_average(self):
         '''
@@ -133,5 +120,5 @@ class Metrics():
         return psutil.net_io_counters()._asdict()
 
     def to_json(self):
-        return json.dumps(self.metrics)
+        return json.dumps(self.__dict__)
 
