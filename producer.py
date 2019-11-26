bd4b0cdc9fb96ec9605467dabbafaa187dda4593
Suan Kan <5026704+suankan@users.noreply.github.com>
2019-11-26 13:52:47 +1100

    Added cmd arguments to producer. Structured the code into a plain sequence.

--- producer.py
+++ producer.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import uuid
+import argparse
 from metrics import Metrics
 from confluent_kafka import Producer
 
@@ -14,36 +15,45 @@ def delivery_report(err, msg):
     else:
         logging.info(f'Message delivered to {msg.topic()} partition {msg.partition()}')
 
-def run_producer(p, interval):
-    try:
-        while True:
-            logging.info(f'Sleeping {interval} seconds before next metrics collection')
-            time.sleep(interval)
+parser = argparse.ArgumentParser()
+parser.add_argument("--broker", help="Specify Kafka broker servername:port")
+parser.add_argument("--cacert", help="Specify CA certificate to authenticate to Kafka broker")
+parser.add_argument("--cert", help="Specify certificate to authenticate to Kafka broker")
+parser.add_argument("--certkey", help="Specify certificate key to authenticate to Kafka broker")
+parser.add_argument("--topic", help="Specify Kafka topic to write metrics to. Defaults to os-metrics", default="os-metrics")
+parser.add_argument("--interval", help="Specify polling interval for gathering metrics. Defaults to 2 sec", default=2)
+parser.add_argument("--log", help="Specify log file. Defaults to ./tmp/producer.log", default='./tmp/producer.log')
+args = parser.parse_args()
 
-            logging.info(f'Collecting metrics')
-            metrics = Metrics()
+if not args.broker or not args.cacert or not args.cert or not args.certkey:
+    print(f'You must specify --broker, --cacert, --cert, --certkey options to this command')
+    exit(1)
 
-            key = str(uuid.uuid4())
-            topic = "os-metrics"
-            logging.info(f'Sending metrics to Kafka topic {topic}, key {key}')
-            p.produce(topic, key=str(key), value=metrics.to_json(), callback=delivery_report)
-
-            logging.info(f'Wait for all messages in the Producer queue to be delivered.')
-            p.flush()
-    except KeyboardInterrupt:
-        pass
-
-logging.basicConfig(filename='producer.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(filename=args.log, filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
 
 p = Producer({
-    'bootstrap.servers': 'kafka-39b301ca-kansuan-4650.aivencloud.com:14598',
+    'bootstrap.servers': args.broker,
     'client.id': 'test',
     'default.topic.config': {'acks': 'all'},
-
     'security.protocol': 'SSL',
-    'ssl.ca.location': 'certs/ca.pem',
-    'ssl.certificate.location': 'certs/service.cert',
-    'ssl.key.location': 'certs/service.key'
+    'ssl.ca.location': args.cacert,
+    'ssl.certificate.location': args.cert,
+    'ssl.key.location': args.certkey
 })
 
-run_producer(p, 2)
+try:
+    while True:
+        logging.info(f'Sleeping {args.interval} seconds before next metrics collection')
+        time.sleep(args.interval)
+
+        logging.info(f'Collecting metrics')
+        metrics = Metrics()
+
+        key = str(uuid.uuid4())
+        logging.info(f'Sending metrics to Kafka broker {args.broker} topic {args.topic}, key {key}')
+        p.produce(args.topic, key=str(key), value=metrics.to_json(), callback=delivery_report)
+
+        logging.info(f'Wait for all messages in the Producer queue to be delivered.')
+        p.flush()
+except KeyboardInterrupt:
+    pass
